0de527e944c7572fc2231eb4d92a146bd5cdca3a
Greg Savin <greg.savin@sifive.com>
2020-01-24 17:25:37 -0800

    Progress commenting out ARM-specific code

---
+++ architectures/riscv/test_link.c
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#include "mri.h"
+
+int main()
+{
+  __mriInit(NULL);
+  return 0;
+}
---
+++ boards/riscv-sifive-freedom-metal/semihost.c
@@ -0,0 +1,12 @@
+#include <semihost.h>
+
+
+int Semihost_IsDebuggeeMakingSemihostCall(void)
+{
+    return 0;
+}
+
+int Semihost_HandleSemihostRequest(void)
+{
+    return 0;
+}
